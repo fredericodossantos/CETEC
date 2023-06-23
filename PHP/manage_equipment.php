b738de47708b81e62e89a7b7c2b820c51b04f470
fredericodossantos <frederico.dos.santos@gmail.com>
2023-06-23 16:37:36 -0300

    style corrected

--- PHP/manage_equipment.php
+++ PHP/manage_equipment.php
@@ -87,7 +87,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Equipment</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <h1>Manage Equipment</h1>
